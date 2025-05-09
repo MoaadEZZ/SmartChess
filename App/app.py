3d2f11f62502af8e8e3afdea7f6c8f902083c07d
MoaadEZZ <ezzahirmoaad@gmail.com>
2025-05-09 15:29:27 +0100

    first commit

--- App/app.py
+++ App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import chess
 
 app = Flask(__name__)
@@ -6,4 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def main_page():
     board = chess.Board()
-    return "<p>Hello, World!</p>"
+    board_state = {}
+
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece:
+            square_name = chess.square_name(square)
+            color = "white" if piece.color == chess.WHITE else "nigga"
+            piece_name = piece.symbol().lower()
+            board_state[square_name] = f"{color}_{piece_name}.png"
+
+    files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    ranks = list(range(8, 0, -1))
+
+    return render_template("MainView.html", board_state=board_state, files=files, ranks=ranks)
